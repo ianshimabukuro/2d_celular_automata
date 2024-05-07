a1eab8e7e6c740cb3c5e41b6b86c73a6da7a3960
ianshimabukuro <shimakun000@gmail.com>
2024-05-07 19:30:22 +0900

    First commit

--- grid_to_grid_function.py
+++ grid_to_grid_function.py
@@ -8,17 +8,10 @@ def grid2_function(x):
     rule = np.array([0,0,0,1,0,1,0,0])
     for i in range(1,x.shape[0]-1):
         for j in range(1,x.shape[1]-1):
-            if x[i+1][j] == 1:
+            if x[i+1][j] == 1 and x[i-1][j] == 0:
                 x[i][j]=1
-    print(x.shape)
+            else:
+                x[i][j] = 0
+
     return x
 
-a= numpy.random.randint(2,size=(5, 5))
-# Set the border elements to 0
-a[0, :] = 0  # First row
-a[-1, :] = 0  # Last row
-a[:, 0] = 0  # First column
-a[:, -1] = 0  # Last column
-print(a)
-b = grid2_function(a)
-print(b)
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-a= numpy.random.randint(2,size=(5, 5))
+import numpy as np
+from grid_to_grid_function import grid2_function
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import copy
+import imageio
+
+order = 50
+#Initialize the initial matrix
+a= np.random.randint(2,size=(order, order))
+
 # Set the border elements to 0
 a[0, :] = 0  # First row
 a[-1, :] = 0  # Last row
 a[:, 0] = 0  # First column
 a[:, -1] = 0  # Last column
+
+for i in range(0,500):
+    a = grid2_function(a)
+    plt.clf()
+    plt.imshow(a, cmap=plt.get_cmap('Blues'))
+    plt.pause(0.1)
+
 print(a)
 b = grid2_function(a)
 print(b)
