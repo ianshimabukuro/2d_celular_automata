afa7d0fe1ec4cffc9523c3da9bb32dc92d591118
ianshimabukuro <shimakun000@gmail.com>
2024-05-07 20:15:01 +0900

    Added boundary conditions, percentage of alive cells and 8 neighbors instead of 4

--- grid_to_grid_function.py
+++ grid_to_grid_function.py
@@ -4,14 +4,37 @@ import numpy.random
 
 def grid2_function(x):
 
-    iteration = np.array([])
-    rule = np.array([0,0,0,1,0,1,0,0])
     for i in range(1,x.shape[0]-1):
         for j in range(1,x.shape[1]-1):
-            if x[i+1][j] == 1 and x[i-1][j] == 0:
+
+            #Specify the rule
+            n_n = 0
+            if x[i+1][j] == 1:
+                n_n+=1
+            if x[i-1][j] == 1:
+                n_n+=1
+            if x[i][j+1] == 1:
+                n_n+=1
+            if x[i][j-1] == 1:
+                n_n+=1
+            if x[i + 1][j+ 1] == 1:
+                n_n += 1
+            if x[i - 1][j + 1] == 1:
+                n_n += 1
+            if x[i + 1][j - 1] == 1:
+                n_n += 1
+            if x[i - 1][j - 1] == 1:
+                n_n += 1
+
+            if n_n<2 and x[i][j] == 1:
+                x[i][j]=0
+            elif (n_n==2 or n_n==3) and x[i][j] == 1:
                 x[i][j]=1
-            else:
-                x[i][j] = 0
+            elif n_n>3 and x[i][j] == 1:
+                x[i][j]=0
+            elif n_n == 3 and x[i][j] == 0:
+                x[i][j] = 1
+
 
     return x
 
--- main.py
+++ main.py
@@ -6,9 +6,18 @@ from matplotlib import cm
 import copy
 import imageio
 
-order = 50
+order = 100
+percentage = 0.05
 #Initialize the initial matrix
-a= np.random.randint(2,size=(order, order))
+a= np.random.random(size=(order, order))
+for i in range(1, a.shape[0]):
+    for j in range(1, a.shape[1]):
+        if a[i][j] < percentage:
+            a[i][j] = 0
+        else:
+            a[i][j] = 1
+
+
 
 # Set the border elements to 0
 a[0, :] = 0  # First row
@@ -19,9 +28,6 @@ a[:, -1] = 0  # Last column
 for i in range(0,500):
     a = grid2_function(a)
     plt.clf()
-    plt.imshow(a, cmap=plt.get_cmap('Blues'))
+    plt.imshow(a, cmap=plt.get_cmap('Paired'))
     plt.pause(0.1)
 
-print(a)
-b = grid2_function(a)
-print(b)
